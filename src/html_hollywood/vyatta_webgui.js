d40c4ee2737e4f6f13ec870fe2b37a479e3d1d8d
Marat Nepomnyashy <marat@vyatta.com>
2008-05-23 16:53:22 -0700

    Load config nodes into an array before adding to tree.

--- src/html_hollywood/vyatta_webgui.js
+++ src/html_hollywood/vyatta_webgui.js
@@ -1,15 +1,33 @@
 
 YAHOO.namespace("vyatta.webgui");
 
-YAHOO.vyatta.webgui.TemplateNode = function() {
-};
-YAHOO.vyatta.webgui.ConfigNode = function() {
-};
-YAHOO.vyatta.webgui.VyattaNodes = function() {
-};
 
+YAHOO.vyatta.webgui.TemplateNode = function() {
+}
+YAHOO.vyatta.webgui.ConfigNode = function(name) {
+	this.name = name;
+}
+YAHOO.vyatta.webgui.ConfigNode.prototype = {
+	name: null
+}
+
+YAHOO.vyatta.webgui.VyattaNodes = function(node) {
+	this.node = node;
+	this.config = new Array();
+};
 YAHOO.vyatta.webgui.VyattaNodes.prototype = {
-	loadNodes: function(node) {
+
+	config:	null,
+	node:	null,
+	
+	createChildrenTreeNodes: function(onCompleteCallback) {
+		var node_path = YAHOO.vyatta.webgui.getNodePath(this.node);
+		this.loadConfigNodes(node_path, onCompleteCallback);
+	},
+
+	loadConfigNodes: function(node_path, onCompleteCallback) {
+		var cfg = this.config;
+		var n = this.node;
 		var successHandler = function(o) {
 			if (o.responseXML != null && o.responseXML.documentElement != null && o.responseXML.documentElement.childNodes != null) {
 				for (var i = 0; i < o.responseXML.documentElement.childNodes.length; i++) {
@@ -17,9 +35,7 @@ YAHOO.vyatta.webgui.VyattaNodes.prototype = {
 						if (o.responseXML.documentElement.childNodes[i].attributes != null) {
 							var nameAttr = o.responseXML.documentElement.childNodes[i].attributes.getNamedItem("name");
 							if (nameAttr != null) {
-								var nn = new YAHOO.widget.TextNode(nameAttr.value, node);
-								YAHOO.vyatta.webgui.setDynamicLoad(nn);
-								YAHOO.vyatta.webgui.tree.draw();
+								cfg.push(new YAHOO.vyatta.webgui.ConfigNode(nameAttr.value));
 							}
 						}
 					}
@@ -27,6 +43,15 @@ YAHOO.vyatta.webgui.VyattaNodes.prototype = {
 			} else {
 				alert("Server communication error.  Did not receive an XML response.");
 			}
+
+			for (var i in cfg) {
+				var cn = cfg[i];
+				var nn = new YAHOO.widget.TextNode(cn.name, n);
+				YAHOO.vyatta.webgui.setDynamicLoad(nn);
+				YAHOO.vyatta.webgui.tree.draw();
+			}
+			onCompleteCallback();
+
 		}
 		var failureHandler = function(o) {
 			alert("Server communication error.  Error code: " + o.status + "  Description: " + o.statusText);
@@ -35,7 +60,6 @@ YAHOO.vyatta.webgui.VyattaNodes.prototype = {
 			success: successHandler,
 			failure: failureHandler
 		};
-		var node_path = YAHOO.vyatta.webgui.getNodePath(node);
 		var transaction = YAHOO.util.Connect.asyncRequest("POST", "/cgi-bin/webgui_wrapper.cgi", callback, "<vyatta><configuration><id>" + YAHOO.vyatta.webgui.session_id + "</id><node>" + node_path + "</node></configuration></vyatta>\n");
 	}
 }

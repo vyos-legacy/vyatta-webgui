1cf427416b993e924869f3b0743814c756d55e43
An-Cheng Huang <ancheng@vyatta.com>
2008-06-09 19:26:26 -0700

    provide visual indications to differentiate active, set, delete, and unconfigured nodes.

--- src/client/vyatta.js
+++ src/client/vyatta.js
@@ -1,3 +1,66 @@
+VyattaNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
+  getNodeStyle: function(node) {
+    if (node.attributes.configured == undefined) {
+      return ' class="v-node-nocfg" style="color:black;"';
+    } else if (node.attributes.configured == 'active') {
+      return ' class="v-node-active" style="color:black;"';
+    } else if (node.attributes.configured == 'set') {
+      return ' class="v-node-set" style="color:green;"';
+    } else if (node.attributes.configured == 'delete') {
+      return ' class="v-node-delete" style="color:red;"';
+    }
+    return '';
+  },
+
+  renderElements: function(n, a, targetNode, bulkRender) {
+    this.indentMarkup = n.parentNode ? n.parentNode.ui.getChildIndent() : '';
+    var cb = typeof a.checked == 'boolean';
+    var href = a.href ? a.href : Ext.isGecko ? "" : "#";
+    var styleStr = this.getNodeStyle(n);
+    var buf = [
+      '<li class="x-tree-node"><div ext:tree-node-id="', n.id,
+      '" class="x-tree-node-el x-tree-node-leaf x-unselectable ', a.cls,
+      '" unselectable="on">',
+      '<span class="x-tree-node-indent">', this.indentMarkup, "</span>",
+      '<img src="', this.emptyIcon, '" class="x-tree-ec-icon x-tree-elbow" />',
+      '<img src="', a.icon || this.emptyIcon, '" class="x-tree-node-icon',
+      (a.icon ? " x-tree-node-inline-icon" : ""),
+      (a.iconCls ? " "+a.iconCls : ""),'" unselectable="on" />',
+      cb ? ('<input class="x-tree-node-cb" type="checkbox" '
+            + (a.checked ? 'checked="checked" />' : '/>')) : '',
+      '<a hidefocus="on" class="x-tree-node-anchor" href="', href,
+      '" tabIndex="1" ',
+      a.hrefTarget ? ' target="'+a.hrefTarget+'"' : "",
+      '><span unselectable="on"' + styleStr + '>', n.text,
+      "</span></a></div>",
+      '<ul class="x-tree-node-ct" style="display:none;"></ul>', "</li>"
+    ].join('');
+
+    var nel;
+    if(bulkRender !== true && n.nextSibling
+       && (nel = n.nextSibling.ui.getEl())){
+      this.wrap = Ext.DomHelper.insertHtml("beforeBegin", nel, buf);
+    } else {
+      this.wrap = Ext.DomHelper.insertHtml("beforeEnd", targetNode, buf);
+    }
+    this.elNode = this.wrap.childNodes[0];
+    this.ctNode = this.wrap.childNodes[1];
+    var cs = this.elNode.childNodes;
+    this.indentNode = cs[0];
+    this.ecNode = cs[1];
+    this.iconNode = cs[2];
+    var index = 3;
+    if(cb){
+      this.checkbox = cs[3];
+      // fix for IE6
+      this.checkbox.defaultChecked = this.checkbox.checked;
+      index++;
+    }
+    this.anchor = cs[index];
+    this.textNode = cs[index].firstChild;
+  }
+});
+
 Ext.onReady(function(){
     var cookieP = new Ext.state.CookieProvider({
       expires: new Date(new Date().getTime() + (15 * 60 * 1000))
@@ -49,7 +112,7 @@ Ext.onReady(function(){
     });
 
     var jsonGenNode = function(node, nn) {
-      var str = "{text:'" + nn + "'";
+      var str = "{text:'" + nn + "',uiProvider:'node'";
       var q = Ext.DomQuery;
       var nLeaf = q.selectNode('terminal', node);
       if (nLeaf != undefined) {
@@ -58,11 +121,11 @@ Ext.onReady(function(){
         if (nvals.length > 0) {
           var vstr = '';
           for (var i = 0; i < nvals.length; i++) {
-            if (vstr != '') {
-              vstr += ',';
-            }
             var cfgd = q.selectValue('configured', nvals[i], 'NONE');
             if (cfgd == 'active' || cfgd == 'set') {
+              if (vstr != '') {
+                vstr += ',';
+              }
               vstr += "'" + nvals[i].getAttribute('name') + "'";
             }
           }
@@ -164,16 +227,15 @@ Ext.onReady(function(){
         loader: new MyTreeLoader({
             clearOnLoad: true,
             requestMethod: 'POST',
-            dataUrl: '/cgi-bin/webgui-wrap'
-            /*
+            dataUrl: '/cgi-bin/webgui-wrap',
             uiProviders:{
-                'col': Ext.tree.ColumnNodeUI
+                'node': VyattaNodeUI
             }
-            */
         }),
 
         root: new Ext.tree.AsyncTreeNode({
           id: 'root',
+          cls: 'v-node-active',
           text: 'Configuration'
         })
     });

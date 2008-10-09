969ae0b5d220e0fdaf733aced87bd056ea732e37
An-Cheng Huang <ancheng@vyatta.com>
2008-10-09 15:22:18 -0700

    use proper permissions for backup/restore

--- src/server/vyatta-vmbackup.pl
+++ src/server/vyatta-vmbackup.pl
@@ -11,9 +11,7 @@ if ($op eq 'backup') {
   exit 1 if (! -d "$BROOT/$vm"); # vm doesn't exist
   my $stamp = strftime "%Y-%m-%d-%H%M%S", localtime;
   my $fname = "$vm\_$stamp";
-  my $cmd = "cd $BROOT";
-  $cmd .= " ; sg users -c \"tar -czf '$fname.tar.gz' '$vm'\"";
-  system("$cmd");
+  system("cd $BROOT ; tar -czf '$fname.tar.gz' '$vm'");
   exit 1 if (! -f "$BROOT/$fname.tar.gz");
   exit 0;
 } elsif ($op eq 'backup-list') {
@@ -34,15 +32,11 @@ if ($op eq 'backup') {
   exit 1 if (! -f "$BROOT/$file"); # file doesn't exist
   exit 1 if (! -d "$BROOT/$vm"); # vm doesn't exist
   my $tdir = "tmp-$$";
-  my $cmd = "cd $BROOT ; sg users -c ";
-  $cmd .= "\"mkdir $tdir ; cd $tdir ; tar -xzf ../$file\"";
-  system("$cmd");
+  system("cd $BROOT ; mkdir $tdir ; cd $tdir ; tar -xzf ../$file");
   exit 1 if (! -d "$BROOT/$tdir/$vm"); # vm dir doesn't exist
   system("mkdir $BROOT/$vm/.restore >&/dev/null");
   exit 1 if ($? >> 8); # restore in progress
-  $cmd = "sudo mv $BROOT/$tdir/$vm $BROOT/$vm/restore ; ";
-  $cmd .= "sudo chown -R nobody:users $BROOT/$vm/restore";
-  system("$cmd");
+  system("mv $BROOT/$tdir/$vm $BROOT/$vm/restore");
   
   # wait for domU restore
   my ($i, $done) = (0, 0);

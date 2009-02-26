dac4e6c7d7a35cbcf806643b9785f50d04aa13eb
root <root@vyatta.(none)>
2009-02-26 07:38:51 +0000

    1. Move ft_allConfigPanels.js to the sub directory under: ui/config 2. Add more files to ft_allConfigPanels.js, which are:       ft_confEmailServer.js       ft_confTimeServer.js       ft_confBLB.js       ft_confPassword.js       ft_confLDAPserver.js       ft_confMyProfile.js       ft_confUserAdd.js       ft_confUserUpdate.js

---
+++ src/client/FranceTel/js/ui/config/ft_allConfigPanels.js
@@ -0,0 +1,23 @@
+/*
+    Document   : ft_allConfigPanels.js
+    Created on : Mar 02, 2009, 3:18:51 PM
+    Author     : Kevin.Choi
+    Description:
+*/
+
+document.write('<script type="text/javascript" src="js/ui/config/ft_confBaseObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confDashboard.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confVMUpdates.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confRestart.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confHwMonitor.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confUserList.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confBackup.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confFormObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confMyProfile.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confUserAdd.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confUserUpdate.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confEmailServer.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confTimeServer.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confLDAPserver.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confBLB.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/ft_confPassword.js"></script>');

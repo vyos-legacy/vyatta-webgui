f261a7c30fccf51f6991f313affc7c63b3fe276e
An-Cheng Huang <ancheng@vyatta.com>
2009-03-23 19:40:09 -0700

    add OA VM operation script

--- src/server/lib/perl5/OpenApp/VMMgmt.pm
+++ src/server/lib/perl5/OpenApp/VMMgmt.pm
@@ -9,6 +9,7 @@ our $OPENAPP_VENDOR = 'Vyatta';
 our $OPENAPP_BFORMAT = '1';
 
 my $VMDIR = '/opt/vyatta/etc/gui/VM';
+my $LIBVIRT_DIR = '/opt/vyatta/etc/libvirt';
 my $STATUS_DIR = '/opt/vyatta/var/run/vmstatus';
 my $HWMON_FILE = '/opt/vyatta/var/run/vm-monitor.hw';
 
@@ -187,6 +188,11 @@ sub getImgVer {
 sub getDisplayName {
   my ($self) = @_;
   return $self->{_vmDisplayName};
+}
+
+sub getLibvirtCfg {
+  my ($self) = @_;
+  return "$LIBVIRT_DIR/$self->{_vmId}.xml";
 }
 
 ### getters for VM status

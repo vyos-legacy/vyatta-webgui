acd5a9100109b5efc98e0f7a2acdac7fcce99fb1
Kevin Choi <kevin.choi@vyatta.com>
2009-04-09 18:34:34 +0000

    * fixed images dir to support France button images. * removed index file to parent directory

--- src/client/FranceTel/js/ui/config/ft_confUserList.js
+++ src/client/FranceTel/js/ui/config/ft_confUserList.js
@@ -103,7 +103,7 @@ function FT_confUserList(name, callback, busLayer)
                     g_lang.m_ulClick2Edit + " (" + fName + ")");
             var email = ul[3] != undefined ?
                     thisObj.f_renderAnchorHref(
-                    '<img src="images/ico_mail.gif">',
+                    '<img src="' + g_lang.m_imageDir + 'ico_mail.gif">',
                     "mailto:" + ul[3],
                     g_lang.m_ulSendEmail + fName + ' at ' +
                     ul[3]) : "";

4828acfc7ee95197a0023597049faef825b563fe
Michael Larson <mike@ft1.vyatta.com>
2009-04-15 08:32:52 -0700

    fix for session reuse by different user accounts in server

--- src/server/authenticate.cc
+++ src/server/authenticate.cc
@@ -267,22 +267,30 @@ Authenticate::reuse_session()
 
   DIR *dp;
   struct dirent *dirp;
+  string id_str;
   if ((dp = opendir(WebGUI::VYATTA_MODIFY_DIR.c_str())) == NULL) {
     return 0;
   }
 
-  unsigned long id = 0;
   while ((dirp = readdir(dp)) != NULL) {
     if (strncmp(dirp->d_name, ".vyattamodify_", 14) == 0) {
-      string id_str = string(dirp->d_name).substr(14,24);
-      id = strtoul(id_str.c_str(),NULL,10);
-      string user = WebGUI::get_user(id);
-      if (user == _proc->get_msg()._user) {
-	break;
+      string tmp = WebGUI::VYATTA_MODIFY_DIR + string(dirp->d_name);
+      FILE *fp = fopen(tmp.c_str(),"r");
+      if (fp) {
+        char buf[1025];
+        //read value in here....                                                
+        if (fgets(buf, 1024, fp) != 0) {
+          if (string(buf) == _proc->get_msg()._user) {
+            id_str = string(dirp->d_name).substr(14,24);
+            break;
+          }
+        }
+        fclose(fp);
       }
     }
-  }  
-  return id;
+  }
+  closedir(dp);
+  return strtoul(id_str.c_str(),NULL,10);
 }
 
 /**                                                                                                                                               

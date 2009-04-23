2254c0a49c1c9ae89e70d250b8a816f9d51badf6
Kevin Choi <kevin.choi@vyatta.com>
2009-04-23 00:18:00 +0000

    * Instead of hard-code the install new VM and remove VM URL link to the page. I changed it to be setup via OA WUI config file.

--- src/client/FranceTel/ft_oaConfig.js
+++ src/client/FranceTel/ft_oaConfig.js
@@ -14,5 +14,18 @@ var g_oaConfig =
     ////////////////////////////////////////////////////////////////////////
     // un-deploy PBX html file. OA WUI will display this file if PBX is not
     // deployed.
-    m_pbxURI : 'ft_undeployPBXInfo.html'
+    m_pbxURI : 'ft_undeployPBXInfo.html',
+
+    ////////////////////////////////////////////////
+    // URL link to subscribe for install a new application
+    m_installNewVM : "http://www.orange-business.com/en/mnc2/footer/about/regions/north-america/index.html",
+
+    /////////////////////////////////////////////////
+    // URL link to subscribe for remove application
+    m_removeVM : "http://www.orange-business.com/en/mnc2/footer/about/regions/north-america/index.html",
+
+
+    /////////////////////////////////////////
+    // please do not edit beyong this line
+    m_dummy : 'dummy'
 }

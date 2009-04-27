7a9210d2a8aa82c8a27f8ccbe839cd55826444b1
Loi Vo <loi.vo@vyatta.com>
2009-04-27 19:35:02 +0000

    1. Re-checkin missing files.

---
+++ src/client/utm/js/ui/config/utm_allConfigPanels.js
@@ -0,0 +1,16 @@
+/*
+    Document   : utm_allConfigPanels.js
+    Created on : Mar 02, 2009, 3:18:51 PM
+    Author     : Loi.Vo
+    Description:
+*/
+
+document.write('<script type="text/javascript" src="js/ui/config/utm_confBaseObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confFormDefObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confFormObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confvpnOverview.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confVpnRemoteUsrGrp.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confVpnRemoteUsrAdd.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confVpnS2SE.js"></script>');
+
+

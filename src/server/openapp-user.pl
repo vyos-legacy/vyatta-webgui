3d9a0a99cc313463a1760b16d7991ec9d9bb7213
slioch <slioch@eng-140.vyatta.com>
2009-05-07 09:52:58 -0700

    fixed REST message on user deletion, REST messages are now being sent to all domUs

--- src/server/openapp-user.pl
+++ src/server/openapp-user.pl
@@ -227,21 +227,20 @@ sub del_user {
     }
     else {
 	system("ldapdeleteuser $delete");
-
 	my @VMs = OpenApp::VMMgmt::getVMList();
-	my $vm = new OpenApp::VMMgmt($rights);
-	my $ip = undef;
-	if (defined($vm)) {
-	    $ip = $vm->getIP();
-	}
-	if (defined $ip && $ip ne '') {
-	    my $cmd = "http://$ip/notifications/users/$delete";
-	    my $err = OpenApp::Rest::send("DELETE",$cmd);
-	    if ($err != 0) {
-		`logger 'Rest notification error in response from $ip when deleting user $delete'`;
+	for my $vid (@VMs) {
+	    my $vm = new OpenApp::VMMgmt($vid);
+	    if (defined($vm)) {
+		my $ip = $vm->getIP();
+		if (defined $ip && $ip ne '') {
+		    my $cmd = "http://$ip/notifications/users/$delete";
+		    my $err = OpenApp::Rest::send("DELETE",$cmd);
+		    if ($err != 0) {
+			`logger 'Rest notification error in response from $ip when deleting user $delete'`;
+		    }
+		}
 	    }
 	}
-
     }
 }
 

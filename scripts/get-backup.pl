db7bec8ac3bace51b4e2321771408c51d32b5c50
slioch <slioch@eng-140.vyatta.com>
2009-05-08 16:30:14 -0700

    rework of utm backup sequence on utm

---
+++ scripts/get-backup.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl 
+
+use CGI; 
+my $cgi = new CGI; 
+
+
+my $conf = $cgi->param('conf');
+my $data = $cgi->param('data');
+
+
+
+#need to set header here for redirect
+print $cgi->redirect("http://192.168.1.1/archive/utm.tar");

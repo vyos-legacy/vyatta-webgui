4527c67c147076f829c7f05bc080a6f6b6bff06a
slioch <slioch@eng-140.vyatta.com>
2009-05-13 13:56:48 -0700

    remove unused second set of rest parameters on bu

--- scripts/get-backup.pl
+++ scripts/get-backup.pl
@@ -4,10 +4,6 @@ use CGI;
 my $cgi = new CGI; 
 
 
-my $conf = $cgi->param('config');
-my $data = $cgi->param('data');
-
-
 
 #need to set header here for redirect
 print $cgi->redirect("http://192.168.1.1/archive/utm.tar");

4538274f8d55fff4504e006a0d770fc3d2887922
Loi Vo <loi.vo@vyatta.com>
2009-06-04 18:42:30 +0000

    1. Add utm_allNetConfConfigPanels.js to hold all the network configuration screens 2. Add DNS screen

---
+++ src/client/utm/js/ui/config/utm_allNetConfConfigPanels.js
@@ -0,0 +1,22 @@
+/*
+    Document   : utm_allNetConfConfigPanels.js
+    Created on : Mar 02, 2009, 3:18:51 PM
+    Author     : Loi.Vo
+    Description:
+*/
+
+document.write('<script type="text/javascript" src="js/ui/config/utm_confBaseObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confBaseObjExt.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confFormDefObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confFormObj.js"></script>');
+document.write('<script type="text/javascript" src="js/ui/config/utm_confDNS.js"></script>');
+
+
+
+
+
+
+
+
+
+

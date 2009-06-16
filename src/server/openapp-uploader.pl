ae8ac9cef40ddbb06b82956ce2ff519c2abb4126
slioch <slioch@eng-140.vyatta.com>
2009-06-16 14:03:06 -0700

    validate file parameter before creating file on disk

--- src/server/openapp-uploader.pl
+++ src/server/openapp-uploader.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl 
+use lib "/opt/vyatta/share/perl5";
 use CGI; 
+use Vyatta::TypeChecker;
 my $cgi = new CGI; 
 my $file = $cgi->param('mypcfile');
+if (!Vyatta::TypeChecker::validate_restrictive_filename($file)) {
+    exit 1;
+}
 `touch /tmp/$file`;
 open(LOCAL, ">/tmp/$file") or die $!; 
 while(<$file>) { 

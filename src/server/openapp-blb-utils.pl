4fefe6f3b63cfb709f84106b7c05e7c8e178085d
An-Cheng Huang <ancheng@vyatta.com>
2009-07-09 13:29:51 -0700

    add more blb utils

--- src/server/openapp-blb-utils.pl
+++ src/server/openapp-blb-utils.pl
@@ -6,14 +6,15 @@ use lib '/opt/vyatta/share/perl5';
 use OpenApp::LdapUser;
 use OpenApp::BLB;
 
-my ($pexists, $user, $setp, $authb, $issa)
-  = (undef, undef, undef, undef, undef);
+my ($pexists, $user, $setp, $authb, $issa, $bset)
+  = (undef, undef, undef, undef, undef, undef);
 GetOptions(
   'pass-exists' => \$pexists,
   'user=s' => \$user,
   'set-pass' => \$setp,
   'auth-blb=s' => \$authb,
-  'is-standalone' => \$issa
+  'is-standalone' => \$issa,
+  'blb-setting' => \$bset
 );
 
 if (defined($pexists)) {
@@ -41,14 +42,28 @@ if (defined($authb)) {
 }
 
 if (defined($issa)) {
-  # NOTE cannot use BLB::isStandalone() since we don't have a session
-  my $cfg = new Vyatta::Config;
-  $cfg->{_active_dir_base} = '/opt/vyatta/config/active';
-  exit 0 if (!$cfg->existsOrig("$OpenApp::BLB::OA_BLB_CONF_ROOT"));
-  exit 1;
+  exit (isStandalone() ? 0 : 1);
+}
+
+if (defined($bset)) {
+  if (isStandalone()) {
+    print "sa  ";
+    exit 0;
+  }
+  my ($blb_ip, $err) = OpenApp::BLB::getBLBIP();
+  exit 1 if (defined($err));
+  print "blb $blb_ip";
+  exit 0;
 }
 
 exit 1;
+
+sub isStandalone {
+  # NOTE cannot use BLB::isStandalone() since we don't have a session
+  my $cfg = new Vyatta::Config;
+  $cfg->{_active_dir_base} = '/opt/vyatta/config/active';
+  return (!$cfg->existsOrig("$OpenApp::BLB::OA_BLB_CONF_ROOT"));
+}
 
 sub checkPasswordExists {
   my ($user) = @_;

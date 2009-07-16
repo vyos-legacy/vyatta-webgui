4d3fdf3b9a81222960cc7822da9ced10ca9782a8
Loi Vo <loi.vo@vyatta.com>
2009-07-16 23:32:34 +0000

    1. Add more validation. 2. Add sorting functionality. 3. Fix update case.

---
+++ src/client/utm/utm_nwConfig.js
@@ -0,0 +1,17 @@
+/*
+    Document   : utm_nwConfig.js
+    Created on : Mar 02, 2009, 6:18:51 PM
+    Author     : Loi.Vo
+    Description: WUI configuration file for UTM network configuration 
+*/
+
+var g_nwConfig =
+{
+    ///////////////////////////////////////
+    // Number of rows per paging.
+    m_nwMaxDHCPresevedIP : 20,     // maximum of 20 per reserved IP for DHCP
+
+    /////////////////////////////////////////
+    // please do not edit beyong this line
+    m_dummy : 'dummy'
+}

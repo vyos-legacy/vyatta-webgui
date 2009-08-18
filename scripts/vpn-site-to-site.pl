82e8af40ace2fbe083948f8955c16d54625f59bc
slioch <slioch@eng-140.vyatta.com>
2009-08-18 10:57:14 -0700

    rework of vpn support

--- scripts/vpn-site-to-site.pl
+++ scripts/vpn-site-to-site.pl
@@ -31,7 +31,7 @@ use File::Copy;
 use Getopt::Long;
 use XML::Simple;
 
-my ($set,$get,$disable);
+my ($set,$get,$disable,$delete);
 
 
 
@@ -84,10 +84,19 @@ sub execute_set {
 #
 ##########################################################################
 sub execute_disable {
-    my ($opt) = @_;
-    my $peerip = $opt->{peerip}->[0];
-    my $tunnelname = $opt->{tunnelname}->[0];
-    my $disable = $opt->{disable}->[0];
+    my ($s) = @_;
+    #let's use the xmlin parser here to handle this.
+    $s =~ s/^\s+//;
+
+    if (!defined $s) {
+	print ("<form name='' code=1></form>");
+	exit 1;
+    }
+    my $xs = new XML::Simple(forcearray=>1);
+    my $opt = $xs->XMLin($s);
+    my $peerip = $opt->{"peerip"}->[0];
+    my $tunnelname = $opt->{"tunnelname"}->[0];
+    my $disable = $opt->{"disable"}->[0];
 
     if (!defined $peerip || !defined $tunnelname || !$disable) {
 	print ("<form name='disable' code=1></form>");
@@ -96,7 +105,7 @@ sub execute_disable {
     # set up config session
     my $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin");
     if ($err != 0) {
-	print("<form name='expert' code=2></form>");
+	print("<form name='disable' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
 	exit 1;
     }
@@ -113,13 +122,13 @@ sub execute_disable {
     }
     $err = system($cmd);
     if ($err != 0) {
-	print("<form name='expert' code=2></form>");
+	print("<form name='disable' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
 	exit 1;
     }
 
     if ($err != 0) {
-	print("<form name='expert' code=2></form>");
+	print("<form name='disable' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
 	exit 1;
     }
@@ -127,7 +136,70 @@ sub execute_disable {
     # apply config command
     $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit");
     if ($err != 0) {
-	print("<form name='expert' code=2></form>");
+	print("<form name='disable' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    # set up config session
+    system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+
+}
+
+
+##########################################################################
+#
+# execute_delete
+#
+##########################################################################
+sub execute_delete {
+    my ($s) = @_;
+    #let's use the xmlin parser here to handle this.
+    $s =~ s/^\s+//;
+
+    if (!defined $s) {
+	print ("<form name='' code=1></form>");
+	exit 1;
+    }
+    my $xs = new XML::Simple(forcearray=>1);
+    my $opt = $xs->XMLin($s);
+    my $peerip = $opt->{"peerip"}->[0];
+    my $tunnelname = $opt->{"tunnelname"}->[0];
+
+    if (!defined $peerip || !defined $tunnelname) {
+	print ("<form name='disable' code=1></form>");
+    }
+
+    # set up config session
+    my $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin");
+    if ($err != 0) {
+	print("<form name='delete' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    # apply config command
+    my $thash = hash_code($tunnelname);
+
+    my $cmd = "/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete vpn ipsec site-to-site peer $peerip tunnel $thash disable";
+
+    $err = system($cmd);
+    if ($err != 0) {
+	print("<form name='delete' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    if ($err != 0) {
+	print("<form name='delete' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    # apply config command
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit");
+    if ($err != 0) {
+	print("<form name='delete' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
 	exit 1;
     }
@@ -160,66 +232,61 @@ sub execute_set_expert {
     my $ikeauth = $opt->{expert}->[0]->{ikeauth}->[0];
     my $espauth = $opt->{expert}->[0]->{espauth}->[0];
 
-    if (!defined $peerip ||
-	!defined $presharedkey ||
-	!defined $tunnelname ||
-	!defined $lnet ||
-	!defined $rnet ||
-	!defined $type || 
-	!defined $emode ||
-	!defined $ikeencrypt ||
-	!defined $espencrypt ||
-	!defined $dhgroup ||
-	!defined $ikeltime ||
-	!defined $espltime ||
-	!defined $ikeauth ||
-	!defined $espauth) {
+    my $err_str = '';
+    if (!defined $tunnelname) {
+	$err_str .= "<key>tunnelname</key>";
+    }
+    if (!defined $peerip) {
+	$err_str .= "<key>peerip</key>";
+    }
+    if (!defined $presharedkey) {
+	$err_str .= "<key>presharedkey</key>";
+    }
+    if (!defined $lnet) {
+	$err_str .= "<key>lnet</key>";
+    }
+    if (!defined $rnet) {
+	$err_str .= "<key>rnet</key>";
+    }
+    if (!defined $type) {
+	$err_str .= "<key>type</key>";
+    }
+    if (!defined $emode) {
+	$err_str .= "<key>emode</key>";
+    }
+    if (!defined $ikeencrypt && ($ikeencrypt ne '3des' || $ikeencrypt ne 'aes128' || $ikeencrypt ne 'aes256')) {
+	$err_str .= "<key>ikeencrypt</key>";
+    }
+    if (!defined $espencrypt && ($espencrypt ne '3des' || $espencrypt ne 'aes128' || $espencrypt ne 'aes256')) {
+	$err_str .= "<key>espencrypt</key>";
+    }
+    if (!defined $dhgroup && ($dhgroup ne '2' || $dhgroup ne '5')) {
+	$err_str = "<key>dhgroup</key>";
+    }
+    if (!defined $ikeltime) {
+	$err_str .= "<key>ikeltime</key>";
+    }
+    if (!defined $espltime) {
+	$err_str .= "<key>espltime</key>";
+    }
+    if (!defined $ikeauth && ($ikeauth ne 'md5' || $ikeauth ne 'sha1')) {
+	$err_str .= "<key>ikeauth</key>";
+    }
+    if (!defined $espauth && ($espauth ne 'md5' || $espauth ne 'sha1')) {
+	$err_str .= "<key>espauth</key>";
+    }
+
+    if ($err_str ne '') {
 	print ("<form name='expert' code=1>");
-	if (!defined $tunnelname) {
-	    print("<key>tunnelname</key>");
-	}
-	if (!defined $peerip) {
-	    print ("<key>peerip</key>");
-	}
-	if (!defined $presharedkey) {
-	    print ("<key>presharedkey</key>");
-	}
-	if (!defined $lnet) {
-	    print ("<key>lnet</key>");
-	}
-	if (!defined $rnet) {
-	    print ("<key>rnet</key>");
-	}
-	if (!defined $type) {
-	    print ("<key>type</key>");
-	}
-	if (!defined $emode) {
-	    print ("<key>emode</key>");
-	}
-	if (!defined $ikeencrypt) {
-	    print ("<key>ikeencrypt</key>");
-	}
-	if (!defined $espencrypt) {
-	    print ("<key>espencrypt</key>");
-	}
-	if (!defined $dhgroup) {
-	    print ("<key>dhgroup</key>");
-	}
-	if (!defined $ikeltime) {
-	    print ("<key>ikeltime</key>");
-	}
-	if (!defined $espltime) {
-	    print ("<key>espltime</key>");
-	}
-	if (!defined $ikeauth) {
-	    print ("<key>ikeauth</key>");
-	}
-	if (!defined $espauth) {
-	    print ("<key>espauth</key>");
-	}
+	print $err_str;
 	print ("</form>");
 	exit 1;
     }
+
+
+
+
+    my $h = hash_code($tunnelname);
 
     # set up config session
     my $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin");
@@ -230,7 +297,7 @@ sub execute_set_expert {
     }
 
     # apply config command
-    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec esp-group esp_1 proposal 1");
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec esp-group esp_expert_$h proposal 1 encryption $espencrypt");
     if ($err != 0) {
 	print("<form name='expert' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
@@ -238,7 +305,7 @@ sub execute_set_expert {
     }
 
     # apply config command
-    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec ike-group ike_1 proposal 1");
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec esp-group esp_expert_$h proposal 1 hash $espauth");
     if ($err != 0) {
 	print("<form name='expert' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
@@ -246,7 +313,30 @@ sub execute_set_expert {
     }
 
     # apply config command
-    my $h = hash_code($tunnelname);
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec ike-group ike_expert_$h proposal 1 encryption $ikeencrypt");
+    if ($err != 0) {
+	print("<form name='expert' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    # apply config command
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec ike-group ike_expert_$h proposal 1 hash $ikeauth");
+    if ($err != 0) {
+	print("<form name='expert' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    # apply config command
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec ike-group ike_expert_$h proposal 1 dh-group $dhgroup");
+    if ($err != 0) {
+	print("<form name='expert' code=2></form>");
+	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
+	exit 1;
+    }
+
+    # apply config command
     $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec site-to-site peer $peerip tunnel $h");
     if ($err != 0) {
 	print("<form name='expert' code=2></form>");
@@ -292,7 +382,7 @@ sub execute_set_expert {
 
 
     # apply config command
-    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec site-to-site peer $peerip ike-group ike_1");
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec site-to-site peer $peerip ike-group ike_expert_$h");
     if ($err != 0) {
 	print("<form name='expert' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
@@ -300,7 +390,7 @@ sub execute_set_expert {
     }
 
     # apply config command
-    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec site-to-site peer $peerip tunnel $h esp-group esp_1");
+    $err = system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec site-to-site peer $peerip tunnel $h esp-group esp_expert_$h");
     if ($err != 0) {
 	print("<form name='expert' code=2></form>");
 	system("/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end");
@@ -511,6 +601,18 @@ sub execute_set_easy {
 ##########################################################################
 sub execute_get {
     #pull these values from the configuration
+
+    #these are used to grab a specific instance.
+    my ($s) = @_;
+    $s =~ s/^\s+//;
+    #let's use the xmlin parser here to handle this.
+    if (defined $s && $s ne '') {
+	my $xs = new XML::Simple(forcearray=>1);
+	my $opt = $xs->XMLin($s);
+	my $peerip = $opt->{"peerip"}->[0];
+	my $tunnelname = $opt->{"tunnelname"}->[0];
+    }
+
     my @values;
     my $out;
     $out = `/opt/vyatta/sbin/vyatta-output-config.pl vpn ipsec site-to-site`;
@@ -524,7 +626,7 @@ sub execute_get {
 	$ct++;
 	if ($v eq 'peer') {
 	    if ($tmp ne '') {
-		print "<site-to-site><$type>$tmp</$type></site-to-site>";
+		print "<site-to-site><$type>$tmp</$type><status>tbd</status></site-to-site>";
 		$tmp = "";
 	    }
 	    $tmp .= "<peerip>$values[$ct]</peerip>";
@@ -547,8 +649,8 @@ sub execute_get {
 	elsif ($v eq 'pre-shared-secret') {
 	    $tmp .= "<presharedkey>$values[$ct]</presharedkey>";
 	}
-	elsif ($v eq 'enable') {
-	    $tmp .= "<enable>$values[$ct]</enable>";
+	elsif ($v eq 'disable') {
+	    $tmp .= "<disable/>";
 	}
     }
 
@@ -566,9 +668,10 @@ sub execute_get {
 #
 ##########################################################################
 sub usage() {
-    print "       $0 --set <key>key</key><value>value</value><key>key</key><value>value</value>...>\n";
-    print "       $0 --disable <key>key</key><value>value</value><key>key</key><value>value</value>...>\n";
+    print "       $0 --set <key>key</key><value>value</value><key>key</key><value>value</value>...>\n";    
     print "       $0 --get [tunnelname]\n";
+    print "       $0 --delete <key>peer</key><tunnelname>name</tunnelname>\n";
+    print "       $0 --disable <key>peer</key><tunnelname>name</tunnelname><disable>true|false</disable>\n";
     exit 0;
 }
 
@@ -576,6 +679,7 @@ sub usage() {
 GetOptions(
     "set=s"                => \$set,
     "get:s"                => \$get,
+    "delete=s"             => \$delete,
     "disable=s"            => \$disable,
     ) or usage();
 
@@ -583,7 +687,10 @@ if (defined $set ) {
     execute_set($set);
 }
 elsif (defined $disable) {
-    execute_enable($disable);
+    execute_disable($disable);
+}
+elsif (defined $delete) {
+    execute_disable($delete);
 }
 else {
     execute_get($get);

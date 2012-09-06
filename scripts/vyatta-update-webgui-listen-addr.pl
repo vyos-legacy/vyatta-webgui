4e13d5b0475d7fec56c10616dc45e04f86376f1f
John Southworth <john.southworth@vyatta.com>
2012-09-06 15:29:47 -0700

    Bugfix: 7504, 7510 Fix some webgui listen address problems

--- scripts/vyatta-update-webgui-listen-addr.pl
+++ scripts/vyatta-update-webgui-listen-addr.pl
@@ -19,6 +19,7 @@ use Vyatta::Config;
 my $config = new Vyatta::Config;
 
 my @addrs = $config->returnValues("service https listen-address");
+my $http_redir = $config->returnValue("service https http-redirect");
 
 open my $fp, ">", "/etc/lighttpd/conf-enabled/10-ssl.conf";
 
@@ -29,25 +30,40 @@ print $fp "##http://www.lighttpd.net/documentation/ssl.html \n";
 print $fp "\n";
 print $fp "#### SSL engine\n";
 print $fp "\n";
+print $fp "ssl.engine                = \"enable\"\n";
+print $fp "ssl.pemfile               = \"/etc/lighttpd/server.pem\"\n";
+print $fp "ssl.use-sslv2             = \"disable\"\n";
 
 if ($#addrs >= 0) {
-    foreach my $addr (@addrs) {
-	if ($addr =~ /:/) {
-	    print $fp "\$SERVER[\"socket\"] == \"[$addr]:443\" {\n";
+	print $fp "server.bind               = \"127.0.0.1\"\n";
+	foreach my $addr (@addrs) {
+		if ($addr =~ /:/) {
+		    print $fp "\$SERVER[\"socket\"] == \"[$addr]:443\" {\n";
+		}
+		else {
+		    print $fp "\$SERVER[\"socket\"] == \"$addr:443\" {\n";
+		}
+		print $fp "                  ssl.engine                  = \"enable\"\n";
+		print $fp "                  ssl.pemfile                 = \"/etc/lighttpd/server.pem\"\n";
+		print $fp "}\n";
+		if (defined($http_redir) && $http_redir eq "enable"){
+			if ($addr =~ /:/) {
+				print $fp "\$SERVER[\"socket\"] == \"[$addr]:80\" {\n";
+			}
+			else {
+				print $fp "\$SERVER[\"socket\"] == \"$addr:80\" {\n";
+			}
+			print $fp "                  ssl.engine                  = \"disable\"\n";
+			print $fp "}\n";
+		}
 	}
-	else {
-	    print $fp "\$SERVER[\"socket\"] == \"$addr:443\" {\n";
-	}
-	print $fp "                  ssl.engine                  = \"enable\"\n";
-	print $fp "                  ssl.pemfile                 = \"/etc/lighttpd/server.pem\"\n";
-	print $fp "}\n";
-    }
 }
 else {
-	print $fp "\$SERVER[\"socket\"] == \"0.0.0.0:443\" {\n";
-	print $fp "                  ssl.engine                  = \"enable\"\n";
-	print $fp "                  ssl.pemfile                 = \"/etc/lighttpd/server.pem\"\n";
-	print $fp "}\n";
+	if (defined($http_redir) && $http_redir eq "enable"){
+		print $fp "\$SERVER[\"socket\"] == \"0.0.0.0:80\" {\n";
+		print $fp "                  ssl.engine                  = \"disable\"\n";
+		print $fp "}\n";
+	}
 }
 
 close $fp;
